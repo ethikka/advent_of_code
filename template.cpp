169877e8cfe676f5d0809eb3ca7d475ab36cd1a3
Timo Bredenoort <ethikka+bitbucket@gmail.com>
2017-12-14 15:49:34 +0100

    template cpp file

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <chrono>
+
+void solve() {
+  std::string line;
+  while (std::getline(std::cin, line)) {
+  }
+  std::cout << "Solution part 1: " << res1 << std::endl << "Solution part 2: " << res2 << std::endl;
+}
+
+int main(void) {
+  auto start_time = std::chrono::high_resolution_clock::now();
+  solve();
+  auto end_time = std::chrono::high_resolution_clock::now();
+  auto ms_count = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+  std::cout << "Ran for " << ms_count << "ms" << std::endl;
+}
+

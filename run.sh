25237de8355e7b49e52f8eeab35e1d9e8cdd3489
Timo Bredenoort <timo@schoolmaster.nl>
2018-11-29 16:02:15 +0100

    Less typing!

--- run.sh
+++ run.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
 #
 # AoC runner
+year=$1
+day=$2
 
-file="./buildoutput/d$1$2.out"
-inputfile="./$1/input.day$2"
-cppfile="./$1/day$2.cpp"
-key="$1 $2"
+if [ ! $1 ]
+then
+  year=`date +"%Y"`
+  day=`date +"%d"`
+fi
+
+file="./buildoutput/d$year$day.out"
+inputfile="./$year/input.day$day"
+cppfile="./$year/day$day.cpp"
+key="$year $day"
 optsfile="ExtraOpts.txt"
 
 extraoptions=`egrep "$key" $optsfile | sed 's/[^ ]* *[^ ]* *//'`
 
 if [ ! -f $file ]
 then
-  echo "No binary: making '$1' '$2' "
-  make $2 YEAR=$1 $extraoptions
+  echo "No binary: making '$year' '$day' "
+  make $day YEAR=$year $extraoptions
 fi
 
 if [ $cppfile -nt $file ]
 then
-  echo "Old binary: making '$1' '$2' "
-  make $2 YEAR=$1 $extraoptions
+  echo "Old binary: making '$year' '$day' "
+  make $day YEAR=$year $extraoptions
 fi
 
 

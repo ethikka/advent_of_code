43638a7631d05c2f7830178ccc5294e346e13729
Timo Bredenoort <ethikka@gmail.com>
2019-12-10 10:51:48 +0100

    refactor

--- 2019/day10.cpp
+++ 2019/day10.cpp
@@ -18,18 +18,16 @@ void solve() {
   int res1(0), res2(99999999);
 
   std::map<int,point> asteroids;
+  std::map<float, std::map<int,point>> bestdirections;
+  std::vector<int> keys;
+
   while (std::cin >> ch) {
     if (ch == '#') 
       asteroids[asteroids.size()] = {pos / 24, pos % 24};
     pos++;
   }
 
-  std::vector<int> keys;
-  for (auto entry : asteroids) {
-    keys.push_back(entry.first);
-  }
-
-  std::map<float, std::map<int,point>> bestdirections;
+  for (auto entry : asteroids) keys.push_back(entry.first);
 
   for (auto a: asteroids) {
     std::map<float, std::map<int,point>> dirs;
@@ -37,24 +35,24 @@ void solve() {
       if (keys[a.first] != keys[t.first])
         dirs[fmod(std::atan2((t.second.y - a.second.y),(t.second.x - a.second.x))*180/M_PI+270, 360)][std::abs(t.second.x - a.second.x)+std::abs(t.second.x - a.second.x)] = t.second;
     int d = dirs.size();
-    if (d > res1)
+    if (d > res1) {
       bestdirections = dirs;
-    res1 = std::max(res1, d);
+      res1 = d;
+    }
   }
 
   float cld = bestdirections.lower_bound(90.0f)->first;
   int destructioncnt(0);
   while (destructioncnt <= 200) {
     if (bestdirections[cld].size() > 0) {
-      if (destructioncnt == 199) {
+      if (destructioncnt == 199) 
         for (auto p: bestdirections[cld]) 
           res2 = std::min(res2, (p.second.y*100)+p.second.x);
-      } else 
-        bestdirections[cld].erase(bestdirections[cld].begin());
+      bestdirections[cld].erase(bestdirections[cld].begin());
       destructioncnt++;
-      std::cout << std::endl;
     }
-    // volgende element in de map
+
+    // vorige element in de map
     auto it = bestdirections.find(cld);
     if (it == bestdirections.begin())
       cld = bestdirections.rbegin()->first;

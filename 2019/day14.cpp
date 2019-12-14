c575a28a0e94e44a18781fadaac318b07f19e487
Timo Bredenoort <ethikka@gmail.com>
2019-12-14 15:34:40 +0100

    refactor

--- 2019/day14.cpp
+++ 2019/day14.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string>
 #include <map>
-#include <vector>
 #include <cmath>
 #include <chrono>
 
@@ -25,18 +24,11 @@ int64_t ore_needed(std::string el, int amount) {
     std::string elem = needed_elements.begin()->first;
     int64_t mult = needed_elements.begin()->second;
     needed_elements.erase(needed_elements.begin());
-    if (elem == "ORE")
-      res += mult;
+    if (elem == "ORE") res += mult;
     else {
-      if (leftovers[elem] > 0) {
-        int64_t left = leftovers[elem];
-        if (mult > left) {
-          mult -= left;
-          leftovers.erase(elem);
-        } else {
-          leftovers[elem] -= mult;
-          mult = 0;
-        }
+      if (leftovers.count(elem) > 0) {
+        mult -= leftovers[elem];
+        leftovers.erase(elem);
       }
       if (mult != 0) {
         int64_t multiplier = ceil((mult*1.0 / reactions[elem].amount*1.0));
@@ -53,12 +45,12 @@ void solve() {
   std::string name, discard;
   int64_t amount;
   int64_t res2(0);
-
   element tmp;
+
   while (std::cin >> amount >> name) {
     if (name.back() == ',') 
       tmp.elements[name.substr(0, name.size()-1)] = amount;
-    if (name.back() != ',') {
+    else  {
       tmp.elements[name] = amount;
       std::cin >> discard >> tmp.amount >> tmp.result;
       reactions[tmp.result] = tmp;
@@ -68,7 +60,7 @@ void solve() {
   }
 
   int idx(2), diff(1000000000);
-  while (true) {
+  while (idx += diff) 
     if (ore_needed("FUEL", idx) > 1000000000000) {
       if (ore_needed("FUEL", idx-1) < 1000000000000) {
         res2 = idx-1;
@@ -76,9 +68,8 @@ void solve() {
       }
       idx -= diff;
       diff /= 2;
-    } else
-      idx += diff;
-  }
+    } 
+  
   std::cout << "Solution part 1: " << ore_needed("FUEL", 1) << std::endl << "Solution part 2: " << res2 << std::endl;
 }
 
@@ -90,4 +81,3 @@ int main(void) {
   auto ms_count = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
   std::cout << "Ran for " << ms_count << "ms" << std::endl;
 }
-

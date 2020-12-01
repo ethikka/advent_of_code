390bbd5eb7be45c3771b5b1cd27ef0a5a6bf8a97
Timo <ethikka+bitbucket@gmail.com>
2020-12-01 07:09:36 +0100

    Day 1: Report Repair

--- 2020/day1.cpp
+++ 2020/day1.cpp
@@ -2,12 +2,26 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <vector>
 
 void solve() {
-  std::string line;
-  int res1(0), res2(0);
-  while (std::getline(std::cin, line)) {
+  int res1(0), res2(0), line(0);
+  std::vector<int> e;
+  while (std::cin >> line) {
+    e.push_back(line);
   }
+
+  for (int ii = 0; ii < e.size()-1; ii++)
+    for (int jj = ii + 1; jj < e.size(); jj++)
+      if (e[ii]+e[jj] == 2020) 
+        res1 = e[ii] * e[jj];
+
+  for (int ii = 0; ii < e.size()-2; ii++)
+    for (int jj = ii + 1; jj < e.size()-1; jj++)
+       for (int kk = jj + 1; kk < e.size(); kk++)
+         if ((e[ii]+e[jj]+e[kk]) == 2020)
+           res2 = e[ii] * e[jj] * e[kk];
+
   std::cout << "Solution part 1: " << res1 << std::endl << "Solution part 2: " << res2 << std::endl;
 }
 

8d24959c8c7c15cd4f7b49cef9a37bf1dc0c7c75
Timo <ethikka+bitbucket@gmail.com>
2020-12-01 15:56:51 +0100

    itertools for c++ >:)

--- 2020/day1.cpp
+++ 2020/day1.cpp
@@ -3,25 +3,21 @@
 #include <string>
 #include <chrono>
 #include <vector>
+#include <cppitertools/combinations.hpp>
 
 void solve() {
   int res1(0), res2(0), line(0);
   std::vector<int> e;
-  while (std::cin >> line) {
+  while (std::cin >> line) 
     e.push_back(line);
+
+  for (auto&& i: iter::combinations(e, 3)) {
+    if (i[0] + i[1] == 2020 && res1 == 0)
+      res1 = i[0] * i[1];
+    if (i[0] + i[1] + i[2] == 2020 && res2 == 0)
+      res2 = i[0] * i[1] * i[2];
   }
-
-  for (int ii = 0; ii < e.size()-1; ii++)
-    for (int jj = ii + 1; jj < e.size(); jj++)
-      if (e[ii]+e[jj] == 2020) 
-        res1 = e[ii] * e[jj];
-
-  for (int ii = 0; ii < e.size()-2; ii++)
-    for (int jj = ii + 1; jj < e.size()-1; jj++)
-       for (int kk = jj + 1; kk < e.size(); kk++)
-         if ((e[ii]+e[jj]+e[kk]) == 2020)
-           res2 = e[ii] * e[jj] * e[kk];
-
+  
   std::cout << "Solution part 1: " << res1 << std::endl << "Solution part 2: " << res2 << std::endl;
 }
 
@@ -30,7 +26,7 @@ int main(void) {
   auto start_time = std::chrono::high_resolution_clock::now();
   solve();
   auto end_time = std::chrono::high_resolution_clock::now();
-  auto ms_count = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-  std::cout << "Ran for " << ms_count << "ms" << std::endl;
+  auto ms_count = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+  std::cout << "Ran for " << ms_count << " microseconds" << std::endl;
 }
 
---
+++ run_once.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# AoC runner
+catch_errors() {
+  exit 1;
+}
+trap catch_errors ERR
+
+year=$1
+day=$2
+
+if [ ! $1 ]
+then
+  year=`date +"%Y"`
+  day=`date +"%d" | sed 's/^0*//'`
+fi
+
+file="./buildoutput/d$year$day.out"
+inputfile="./$year/input.day$day"
+cppfile="./$year/day$day.cpp"
+key="$year $day"
+optsfile="ExtraOpts.txt"
+
+extraoptions=`egrep "$key" $optsfile | sed 's/[^ ]* *[^ ]* *//'`
+clear
+if [ ! -f $file ]
+then
+  echo "No binary: making '$year' '$day' "
+  make $day YEAR=$year $extraoptions
+fi
+
+if [ $cppfile -nt $file ]
+then
+  echo "Old binary: making '$year' '$day' "
+  make $day YEAR=$year $extraoptions
+fi
+
+if [ ! -f $file ]
+then
+  echo "Make failed!"
+else
+  if [ ! -f $inputfile ]
+  then
+    $file 
+  else
+    $file < $inputfile
+  fi
+fi

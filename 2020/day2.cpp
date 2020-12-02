886651163a02203468b95cb15d082154b824236d
Timo <ethikka+bitbucket@gmail.com>
2020-12-02 08:20:10 +0100

    Day 2: Password Philosophy

--- 2020/day2.cpp
+++ 2020/day2.cpp
@@ -2,11 +2,20 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <algorithm>
 
 void solve() {
-  std::string line;
+  std::string quant, chr, pass;
   int res1(0), res2(0);
-  while (std::getline(std::cin, line)) {
+  while (std::cin >> quant >> chr >> pass) {
+    int cnt = std::count(pass.begin(), pass.end(), chr[0]);
+    int pos = quant.find('-');
+    int minv = std::stoi(quant.substr(0, pos));
+    int maxv = std::stoi(quant.substr(pos+1));
+    if (cnt >= minv && cnt <= maxv)
+      res1++;
+    if (!(pass[minv-1] == chr[0]) != !(pass[maxv-1] == chr[0])) 
+      res2++;
   }
   std::cout << "Solution part 1: " << res1 << std::endl << "Solution part 2: " << res2 << std::endl;
 }
@@ -16,7 +25,7 @@ int main(void) {
   auto start_time = std::chrono::high_resolution_clock::now();
   solve();
   auto end_time = std::chrono::high_resolution_clock::now();
-  auto ms_count = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
-  std::cout << "Ran for " << ms_count << "ms" << std::endl;
+  auto ms_count = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+  std::cout << "Ran for " << ms_count << " microseconds" << std::endl;
 }
 

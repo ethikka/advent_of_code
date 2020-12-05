4eea64daf580091ca889591c37afced766df8927
Timo <ethikka+bitbucket@gmail.com>
2020-12-05 11:35:25 +0100

    cleanup day 4

--- 2020/day4.cpp
+++ 2020/day4.cpp
@@ -3,50 +3,41 @@
 #include <map>
 #include "../common/lib.h"
 
-bool between(std::string input, int min, int max) {
-  int inp = std::stoi(input);
-  return (inp >= min && inp <= max);
-}
-
-bool checkhgt(std::string input) {
-  int pos = input.length()-2;
-  if (input.substr(pos, 2) == "cm")
-    return between(input.substr(0, pos), 150, 193);
-  else
-    return between(input.substr(0, pos), 59, 76);
-}
-
-bool checkhcl(std::string input) {
-  if (input.size() != 7) return false;
-  for (int i = 1; i < input.size(); i++)
-    if (!isxdigit(input[i]))
-      return false;
-  return true;
-}
-
-bool checkecl(std::string input) {
-  std::string colors =  " amb blu brn gry grn hzl oth ";
-  return colors.find(" "+input+" ") != std::string::npos;
-}
-
-bool checkpid(std::string input) {
-  if (input.size() != 9) return false;
-  for (int i = 0; i < input.size(); i++)
-    if (!isdigit(input[i]))
-      return false;
-  return true;
-}
-
 struct passport {
   std::map<std::string,std::string> properties;
-  bool isValid() {
-    return ((properties.size() == 7) && properties.count("cid") == 0) || (properties.size() == 8);
+
+  bool checkhgt(std::string input) {
+    int pos = input.length()-2;
+    if (input.substr(pos, 2) == "cm")
+      return between(std::stoi(input.substr(0, pos)), 150, 193);
+    else
+      return between(std::stoi(input.substr(0, pos)), 59, 76);
   }
+
+  bool checkhcl(std::string input) {
+    if (input.size() != 7 || input[0] != '#') return false;
+    for (int i = 1; i < input.size(); i++)
+      if (!isxdigit(input[i]))
+        return false;
+    return true;
+  } 
+
+  bool checkpid(std::string input) {
+    if (input.size() != 9) return false;
+    for (int i = 0; i < input.size(); i++)
+      if (!isdigit(input[i]))
+        return false;
+    return true;
+  } 
+
+  bool between(int inp, int min, int max) { return (inp >= min && inp <= max); }
+  bool checkecl(std::string input)        { return std::string(" amb blu brn gry grn hzl oth ").find(" "+input+" ") != std::string::npos; }
+  bool isValid()                          { return ((properties.size() == 7) && properties.count("cid") == 0) || (properties.size() == 8); }
   bool isValid2() {
-    return (((properties.size() == 7) && properties.count("cid") == 0) || (properties.size() == 8))
-           && between(properties["byr"], 1920, 2002) 
-           && between(properties["iyr"], 2010, 2020) 
-           && between(properties["eyr"], 2020, 2030) 
+    return    isValid()
+           && between(std::stoi(properties["byr"]), 1920, 2002) 
+           && between(std::stoi(properties["iyr"]), 2010, 2020) 
+           && between(std::stoi(properties["eyr"]), 2020, 2030) 
            && checkhgt(properties["hgt"]) 
            && checkhcl(properties["hcl"]) 
            && checkecl(properties["ecl"]) 

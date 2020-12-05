798ef95258c91fb486e14020f3cb53a243762da6
Timo <ethikka+bitbucket@gmail.com>
2020-12-05 11:22:22 +0100

    cleanup

--- 2020/day5.cpp
+++ 2020/day5.cpp
@@ -1,7 +1,6 @@
 #include <sstream>
 #include <cmath>
 #include <set>
-#include <map>
 #include "../common/lib.h"
 
 std::pair<int,int> solve() {
@@ -9,21 +8,18 @@ std::pair<int,int> solve() {
   char inp;
   std::set<int> seats;
   std::string num = "";
-  std::string sublist = "F0B1L0R1";
   while (std::cin >> std::noskipws >> inp) {
     switch (inp) {
-      case '\n': { 
-        seats.insert(std::stoi(num, 0, 2));
-        num = "";
-        break;
-      }
-      default: num += sublist.substr(sublist.find(inp)+1, 1);
+      case '\n': seats.insert(std::stoi(num, 0, 2));
+                 num = "";
+                 break;
+      default:   num += std::string("BR").find(inp) != std::string::npos ? "1": "0";
     }
   }
 
   for(auto iter=seats.begin(), prev=seats.end(); iter != seats.end(); prev=iter, ++iter )
-    if(prev != seats.end())
-      if ((*iter - *prev) > 1) res.second = *prev++;
+    if(prev != seats.end() && (*iter - *prev) > 1) 
+      res.second = *prev + 1;
 
   res.first = *seats.rbegin();
   return res;

ddab9c07ecc8ec9966c75192e2f0545d5918cb52
Timo <ethikka+bitbucket@gmail.com>
2020-12-05 10:01:15 +0100

    <airquotes>cleanup</airquotes>

--- 2020/day5.cpp
+++ 2020/day5.cpp
@@ -1,36 +1,31 @@
 #include <sstream>
 #include <cmath>
 #include <set>
+#include <map>
 #include "../common/lib.h"
 
 std::pair<int,int> solve() {
   std::pair<int,int> res; 
   char inp;
-  int row_up(127), row_low(0), seat_up(7), seat_low(0), max_seat_id(0);
   std::set<int> seats;
+  std::string num = "";
+  std::string sublist = "F0B1L0R1";
   while (std::cin >> std::noskipws >> inp) {
     switch (inp) {
-      case 'F': row_up  = row_up  - ((row_up - row_low + 1)/2); break;
-      case 'B': row_low = row_low + ((row_up - row_low + 1)/2); break;
-      case 'L': seat_up  = seat_up  - ((seat_up - seat_low + 1)/2);break;
-      case 'R': seat_low = seat_low + ((seat_up - seat_low + 1)/2);break;
-      case '\n': 
-        int seat_id = (row_up*8)+seat_up;
-        max_seat_id = std::max(max_seat_id, seat_id);
-        seats.insert(seat_id);
-        row_up = 127; row_low = 0; seat_up = 7; seat_low = 0;
+      case '\n': { 
+        seats.insert(std::stoi(num, 0, 2));
+        num = "";
         break;
+      }
+      default: num += sublist.substr(sublist.find(inp)+1, 1);
     }
   }
 
-  int last(0);
-  for(auto i: seats) {
-    if (++last != i)
-      res.second = last;
-    last = i;
-  }
+  for(auto iter=seats.begin(), prev=seats.end(); iter != seats.end(); prev=iter, ++iter )
+    if(prev != seats.end())
+      if ((*iter - *prev) > 1) res.second = *prev++;
 
-  res.first = max_seat_id;
+  res.first = *seats.rbegin();
   return res;
 }
 

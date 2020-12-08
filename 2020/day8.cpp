3878fb6db0d71bf05a517458040ecfb085c183a4
Timo <ethikka+bitbucket@gmail.com>
2020-12-08 13:17:10 +0100

    minor optimization

--- 2020/day8.cpp
+++ 2020/day8.cpp
@@ -5,7 +5,9 @@
 #include <cmath>
 #include "../common/lib.h"
 
-int run(int fixline, std::vector<std::string> memory) {
+std::vector<std::string> memory;
+
+int run(int fixline) {
   if (fixline >= 0) {
     if      (memory[fixline].substr(0, 3) == "nop") memory[fixline] = "jmp" + memory[fixline].substr(3);
     else if (memory[fixline].substr(0, 3) == "jmp") memory[fixline] = "nop" + memory[fixline].substr(3);
@@ -14,6 +16,10 @@ int run(int fixline, std::vector<std::string> memory) {
   int ip(0), lacc(0);
   while (ip < memory.size()) {
     if (acc.find(ip) != acc.end()) {
+      if (fixline >= 0) { // unfix it
+        if      (memory[fixline].substr(0, 3) == "nop") memory[fixline] = "jmp" + memory[fixline].substr(3);
+        else if (memory[fixline].substr(0, 3) == "jmp") memory[fixline] = "nop" + memory[fixline].substr(3);
+      }
       return fixline == -1 ? lacc: -9999999;
     } else {
       int parm = std::stoi(memory[ip].substr(memory[ip].find(" ")+1));
@@ -30,14 +36,14 @@ std::pair<int,int> solve() {
   std::pair<int,int> res;
 
   std::string line;
-  std::vector<std::string> memory;
   while (std::getline(std::cin, line)) 
     memory.push_back(line);
 
-  res.first = run(-1, memory);
+  res.first = run(-1);
   res.second = -9999999;
   for (int i = 0; i < memory.size() && res.second < 0; i++)
-    res.second = std::max(res.second, run(i, memory));
+    if (memory[i].substr(0, 3) != "acc")
+      res.second = std::max(res.second, run(i));
   return res;
 }
 
--- common/lib.h
+++ common/lib.h
@@ -9,9 +9,18 @@
 
 auto start_time = std::chrono::high_resolution_clock::now();
 
+void reset_start_timer() {
+  start_time = std::chrono::high_resolution_clock::now();
+}
+
+void output_current_timer() {
+  std::cout << std::fixed << std::setprecision(3) << (std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - start_time).count())/1000.0f << "µs" << std::endl;
+}
+
+
 void post_solve(std::pair<int,int> res) {
   std::cout << "Solution part 1: " << res.first << std::endl << "Solution part 2: " << res.second << std::endl;
-  std::cout << std::fixed << std::setprecision(3) << "Ran for " << (std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - start_time).count())/1000.0f << " microseconds" << std::endl;
+  std::cout << std::fixed << std::setprecision(3) << "Ran for " << (std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - start_time).count())/1000.0f << "µs" << std::endl;
 }
 
 #endif

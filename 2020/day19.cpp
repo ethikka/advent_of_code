7d23eb01fcdb7ad8ca810f6f0725e54d3600ddfc
Timo <ethikka+bitbucket@gmail.com>
2020-12-19 10:35:41 +0100

    optimization?

--- 2020/day19.cpp
+++ 2020/day19.cpp
@@ -8,32 +8,25 @@ std::vector<std::string> strings;
 
 std::string generate_regex(int rule, bool part_2)
 {
-  if(part_2)
-  {
-    if (rule == 8) // 8: 42 | 42 8 => Match a continuous sequence of "42"s
-      return '(' + generate_regex(42, part_2) + "+)";
-    if (rule == 11) { // 11: 42 31 | 42 11 31	=> match an equal amount of 42s and 31s next to each other
-        std::string res;
-        res += '(';
-        // my input needs 4 nests, you might want to increase the 4 below ;-)
-        // if you increase it to 7 or beyond you need to #define _GLIBCXX_REGEX_STATE_LIMIT 1000000 before you include <regex>
-        for(int i = 1; i < 4; ++i)
-          res += generate_regex(42, part_2) + '{' + std::to_string(i) + '}' + generate_regex(31, part_2) + '{'+ std::to_string(i) + '}' + '|';
-        res += generate_regex(42, part_2) + generate_regex(31, part_2) + ')';
-        return res;
-    }
+  std::string res("(");
+
+  // 8: 42 | 42 8 => Match a continuous sequence of "42"s
+  if (rule == 8 && part_2)     
+    return '(' + generate_regex(42, part_2) + "+)";
+
+  // 11: 42 31 | 42 11 31	=> match an equal amount of 42s and 31s next to each other, where amount is at least 1
+  if (rule == 11 && part_2) { 
+    for(int i = 1; i < 5; ++i) // maximum amount of nests is 5 (rule 41 and 31 become 8 characters for my input, longest input is 88 charachters, 6 sets would be 96 characters and thus impossible)
+      res += generate_regex(42, part_2) + '{' + std::to_string(i) + '}' + generate_regex(31, part_2) + '{'+ std::to_string(i) + "}|";
+    return res.substr(0, res.size()-1)+')';
   }
 
   std::stringstream cur_rule(rules[rule]);
-  std::string res;
   std::string token;
-  while( cur_rule >> token )
-  {
-    if(std::isdigit(token[0]))
-      res += generate_regex(std::stoi(token), part_2);
-    else res += token;
-  }
-  return '(' + res + ')';
+  while(cur_rule >> token)
+    res += (std::isdigit(token[0]) ? generate_regex(std::stoi(token), part_2): token);
+
+  return res + ')';
 }
 
 std::pair<std::uintmax_t,std::uintmax_t> solve() {
@@ -44,12 +37,11 @@ std::pair<std::uintmax_t,std::uintmax_t> solve() {
   while(std::getline(std::cin, line) && line.size() )
   {
     int pos = line.find_first_of(':');
-    int num = std::stoull(line.substr(0, pos));
+    int num = std::stoi(line.substr(0, pos));
     std::string rule = line.substr(pos + 2);
     rule.erase(std::remove(rule.begin(), rule.end(), '\"'), rule.end());
     rules[num] = rule;
   };
-
   // read strings
   while(std::cin >> line) strings.push_back(line);
 

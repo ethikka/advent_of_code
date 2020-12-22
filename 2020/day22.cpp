7bc138666385de26cd0f064acdde2078c32fd458
Timo <ethikka+bitbucket@gmail.com>
2020-12-22 14:17:21 +0100

    optimizations

--- 2020/day22.cpp
+++ 2020/day22.cpp
@@ -1,6 +1,6 @@
 #include <sstream>
 #include <vector>
-#include <set>
+#include <unordered_set>
 #include <tuple>
 #include "../common/lib.h"
 
@@ -17,13 +17,14 @@ int64_t fingerprint(std::vector<int> deck) {
 }
 
 std::tuple<std::vector<int>,std::vector<int>,int> play_recursive_decks(std::vector<int> player[2], bool recursive, int game) {
-  std::set<std::string> previous_states;
+  std::unordered_set<std::string> previous_states;
   int round(0);
   while (player[0].size() > 0 && player[1].size() > 0) {
     if (recursive) {
-      if (previous_states.count(state(player[0])+"|"+state(player[1]))) 
+      std::string key = state(player[0])+"|"+state(player[1]);
+      if (previous_states.count(key)) 
         return std::make_tuple(player[0], player[1], 0);
-      previous_states.insert(state(player[0])+"|"+state(player[1]));
+      previous_states.insert(key);
     }
 
     int p[2];
@@ -32,22 +33,13 @@ std::tuple<std::vector<int>,std::vector<int>,int> play_recursive_decks(std::vect
       player[i].erase(player[i].begin());
     }
 
+    int winner = (p[0] > p[1] ? 0 : 1);
     if ((p[0] <= player[0].size()) && (p[1] <= player[1].size()) && recursive) {
-      std::vector<int> subgame[2];
-      for(auto i: {0,1}) {
-        for(int n = 0; n < p[i]; n++)
-          subgame[i].push_back(player[i][n]);
-      }
-      std::vector<int> p1, p2;
-      int winner;
+      std::vector<int> p1, p2, subgame[2]{{player[0].begin(), player[0].begin()+p[0]}, {player[1].begin(), player[1].begin()+p[1]}};
       std::tie(p1, p2, winner) = play_recursive_decks(subgame, recursive, game+1);
-      player[winner].push_back(p[winner]);
-      player[winner].push_back(p[(winner+1)%2]);
-    } else {
-      int pn = (p[0] > p[1] ? 0 : 1);
-      player[pn].push_back(p[pn]);
-      player[pn].push_back(p[(pn+1)%2]);
     }
+    player[winner].push_back(p[winner]);
+    player[winner].push_back(p[(winner+1)%2]);
   }
   return std::make_tuple(player[0], player[1], ((player[1].size() == 0) ? 0 : 1 ));
 };
@@ -63,16 +55,14 @@ std::pair<std::uintmax_t,std::uintmax_t> solve() {
   std::pair<std::uintmax_t,std::uintmax_t> res;
   std::string line;
   std::vector<int> player_a[2];
-  std::vector<int> player_b[2];
   for(auto i: {0,1}) {
     std::getline(std::cin, line); // skip player header
-    while(std::getline(std::cin, line) && line.size() ) {
+    while(std::getline(std::cin, line) && line.size())
       player_a[i].push_back(std::stoi(line));
-      player_b[i].push_back(std::stoi(line));
-    }
   }
+  std::vector<int> player_b[2]{player_a[0], player_a[1]};
 
-  res.first = play(player_a, false);  // part a : 32448
-  res.second = play(player_b, true);  // part b : 31680 too low
+  res.first = play(player_a, false);
+  res.second = play(player_b, true);
   return res;
 }

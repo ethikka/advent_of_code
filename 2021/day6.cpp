443d57120b4dc2e69616b9ec9b9363d0784142d2
Timo <ethikka+bitbucket@gmail.com>
2021-12-06 07:44:09 +0100

    less loops

--- 2021/day6.cpp
+++ 2021/day6.cpp
@@ -14,19 +14,13 @@ std::pair<std::uintmax_t,std::uintmax_t> solve() {
   int age(0);
   while (ssl >> age) fish[age]++;
 
-  int64_t nf[9];
   for (int days = 0; days < 256; days++) {
-    for (int ii = 0; ii < 9; ii++) nf[ii] = 0;
-    for (int ii = 0; ii < 9; ii++) {
-      switch (ii) {
-        case 0: nf[8] += fish[ii]; nf[6] = fish[ii]; break;
-        default:
-          nf[ii-1] += fish[ii];
-      }
-    }
-    for (int ii = 0; ii < 9; ii++) fish[ii] = nf[ii];
-    if (days == 79) for (int ii = 0; ii < 9; ii++) res.first += fish[ii];
+    int nf(fish[0]);
+    for (int ii = 0; ii < 8; ii++) fish[ii] = fish[ii+1];
+    fish[6] += fish[8] = nf;
+    
+    if (days == 79) for (auto f: fish) res.first += f;
   }
-  for (int ii = 0; ii < 9; ii++) res.second += fish[ii];
+  for (auto f: fish) res.second += f;
   return res;
 }

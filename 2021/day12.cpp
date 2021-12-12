1b605e9513346645060f7773b4a44210cd492bb0
Timo <ethikka+bitbucket@gmail.com>
2021-12-12 07:51:53 +0100

    less code

--- 2021/day12.cpp
+++ 2021/day12.cpp
@@ -14,15 +14,14 @@ class path {
     std::string last() { return route.back(); }
     bool canVisit(std::string n, bool part_b) {
       int vc = route.size();
-      bool _visited(std::count(route.begin(), route.end(), n) > 0);
       if (vc > 0 && route.back() == "end") return false;
       if (n[0] < 91) route.push_back(n);
-      else if (part_b) {
-        if (_visited && !small_dup && (n != "start") ) {
+      else if (std::count(route.begin(), route.end(), n) == 0) route.push_back(n); 
+      else if (part_b) 
+        if (!small_dup && (n != "start") ) {
           small_dup = true;
           route.push_back(n);
-        } else if (!-_visited) route.push_back(n);
-      } else if (!_visited) route.push_back(n);
+        }
       return route.size() != vc;
     }
 };
@@ -32,31 +31,26 @@ struct work {
   std::string nd;
 };
 
+int64_t run(bool part_b) {
+  path _p;
+  int64_t res(0);
+  std::queue<work> q;
+  q.push({ _p, "start" });
+  while (!q.empty()) {
+    auto w = q.front();
+    q.pop();
+    if (w.nd == "end") res++;
+    if (w.p.canVisit(w.nd, part_b)) for (auto p: possible_paths) if (p.first == w.p.last()) q.push({w.p, p.second});
+  }
+  return res;
+}
+
 std::pair<std::uintmax_t,std::uintmax_t> solve() {
-  std::pair<std::uintmax_t,std::uintmax_t> res;
   std::string line;
   while (std::cin >> line) {
     int p = line.find_first_of('-');
     possible_paths.insert({line.substr(0,p), line.substr(p+1)});
     possible_paths.insert({line.substr(p+1), line.substr(0,p)});
   }
-
-  path _p;
-  std::queue<work> q;
-  q.push({ _p, "start" });
-  while (!q.empty()) {
-    auto w = q.front();
-    q.pop();
-    if (w.nd == "end") res.first++;
-    if (w.p.canVisit(w.nd, false)) for (auto p: possible_paths) if (p.first == w.p.last()) q.push({w.p, p.second});
-  }
-
-  q.push({ _p, "start" });
-  while (!q.empty()) {
-    auto w = q.front();
-    q.pop();
-    if (w.nd == "end") res.second++;
-    if (w.p.canVisit(w.nd, true)) for (auto p: possible_paths) if (p.first == w.p.last()) q.push({w.p, p.second});
-  }
-  return res;
+  return { run(false), run(true)};
 }

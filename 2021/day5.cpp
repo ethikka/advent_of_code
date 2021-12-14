6201f7eeeaca05934b5cafab7686b62bfadcbd42
Timo <ethikka+bitbucket@gmail.com>
2021-12-14 22:42:10 +0100

    even more optimizations

--- 2021/day5.cpp
+++ 2021/day5.cpp
@@ -4,11 +4,6 @@
 #include <map>
 #include "../common/lib.h"
 
-bool check_collinear(int x, int y, int x1, int y1, int x2, int y2){
-  // https://en.wikipedia.org/wiki/Collinearity
-  return (x * (y1 - y2) + x1 * (y2 - y) + x2 * (y - y1)) == 0;
-}
-
 std::pair<std::uintmax_t,std::uintmax_t> solve() {
   std::pair<std::uintmax_t,std::uintmax_t> res;
   std::map<int,int> ventlines_a, ventlines_b;
@@ -20,13 +15,18 @@ std::pair<std::uintmax_t,std::uintmax_t> solve() {
     std::replace(line.begin(), line.end(), ',', ' '); 
     std::istringstream ssl(line);
     ssl >> x1 >> y1 >> discard >> x2 >> y2;
-    for (int xx = std::min(x1,x2); xx <= std::max(x1,x2); xx++) 
-      for (int yy = std::min(y1,y2); yy <= std::max(y1,y2); yy++) {
-        if (x1 == x2 || y1 == y2) ventlines_a[xx+(yy*1000)]++; 
-        if (x1 == x2 || y1 == y2 || check_collinear(xx, yy, x1, y1, x2, y2)) ventlines_b[xx+(yy*1000)]++; 
-      }
+    if      (x1 == x2) for (int yy = std::min(y1,y2); yy <= std::max(y1,y2); yy++) ventlines_a[x1+(yy*10000)]++;
+    else if (y1 == y2) for (int xx = std::min(x1,x2); xx <= std::max(x1,x2); xx++) ventlines_a[xx+(y1*10000)]++;
+    else if (abs(x1-x2) == abs(y1-y2)) {
+      for (int x = 0; x <= abs(x1-x2); x++) 
+        ventlines_b[x1+(x*copysign(1, x2-x1))+((y1+(x*copysign(1, y2-y1)))*10000)]++;
+    };
   }
-  for (auto k: ventlines_a) if (k.second > 1) res.first++;
+  
+  for (auto k: ventlines_a) {
+    ventlines_b[k.first] += k.second;
+    if (k.second > 1) res.first++;
+  }
   for (auto k: ventlines_b) if (k.second > 1) res.second++;
   return res;
 }

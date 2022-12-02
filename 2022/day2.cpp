497eb77348755873364fa8e38061274bb4c6ee3a
Timo <ethikka+bitbucket@gmail.com>
2022-12-02 07:03:43 +0100

    less readable is better? ;-)

--- 2022/day2.cpp
+++ 2022/day2.cpp
@@ -4,16 +4,19 @@
 std::pair<std::uintmax_t,std::uintmax_t> solve() {
   std::pair<std::uintmax_t,std::uintmax_t> res;
   char p1, p2;
+  int p1i, p2i;
   while (std::cin >> p1 >> p2) {
-    if ((p1 == 'A' && p2 == 'Y') || (p1 == 'B' && p2 == 'Z') || (p1 == 'C' && p2 == 'X')) res.first += 6;
-    if ((p1 == 'A' && p2 == 'X') || (p1 == 'B' && p2 == 'Y') || (p1 == 'C' && p2 == 'Z')) res.first += 3;
-    res.first += (int)p2-'X'+1;
-
     int p1i = (int)p1-'A'+1;
-    switch(p2) {
-      case 'X': res.second += 0 + p1i-1; if (p1i == 1) res.second += 3; break;
-      case 'Y': res.second += 3 + p1i; break;
-      case 'Z': res.second += 6 + p1i+1; if (p1i == 3) res.second -= 3; break;
+    int p2i = (int)p2-'X'+1;
+
+    if (((p2i-p1i) == 1) || ((p2i-p1i) == -2)) res.first += 6;
+    if (p1i == p2i) res.first += 3;
+    res.first += p2i;
+
+    switch(p2i) {
+      case 1: res.second += 0 + p1i-1; if (p1i == 1) res.second += 3; break;
+      case 2: res.second += 3 + p1i; break;
+      case 3: res.second += 6 + p1i+1; if (p1i == 3) res.second -= 3; break;
     }
   }
   return res;

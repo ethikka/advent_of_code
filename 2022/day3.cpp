3def16c9121de2d70f8a57f6530f92d967700df8
Timo <ethikka+bitbucket@gmail.com>
2022-12-03 07:48:17 +0100

    ..

--- 2022/day3.cpp
+++ 2022/day3.cpp
@@ -8,13 +8,15 @@
 std::pair<std::uintmax_t,std::uintmax_t> solve() {
   std::pair<std::uintmax_t,std::uintmax_t> res;
   std::string line;
-  std::vector<std::string> badgedetector;
+  std::vector<std::set<char>> badgedetector;
 
   const std::string priorities = ".abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   while (std::cin >> line) {
     std::set<char> left, right, v;
-    badgedetector.push_back(line);
+    std::set<char> uch;
+    for(auto c: line) uch.emplace(c);
+    badgedetector.push_back(uch);
 
     for (int i = 0; i < (line.length()/2); i++) {
       left.emplace(line[i]);
@@ -26,13 +28,7 @@ std::pair<std::uintmax_t,std::uintmax_t> solve() {
 
     if (badgedetector.size() == 3) {
       std::map<char,int> pp;
-      for(int i = 0; i < 3; i++) {
-        std::set<char> uch;
-        for(auto c: badgedetector.at(i)) uch.emplace(c);
-        for(auto c: uch) pp[c]++;
-        uch.clear();
-      }
-
+      for(int i = 0; i < 3; i++) for(auto c: badgedetector.at(i)) pp[c]++;
       for (auto p: pp) if (p.second == 3) res.second += priorities.find(p.first);
       pp.clear();
       badgedetector.clear();

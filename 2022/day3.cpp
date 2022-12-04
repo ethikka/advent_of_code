268cdef367ffe92f1ed5b1b98a628573072a636c
Timo <ethikka+bitbucket@gmail.com>
2022-12-04 15:30:20 +0100

    OPTIMIZE =)

--- 2022/day3.cpp
+++ 2022/day3.cpp
@@ -8,29 +8,26 @@
 std::pair<std::uintmax_t,std::uintmax_t> solve() {
   std::pair<std::uintmax_t,std::uintmax_t> res;
   std::string line;
-  std::vector<std::set<char>> badgedetector;
+  std::vector<std::string> badgedetector;
 
   const std::string priorities = ".abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
   while (std::cin >> line) {
-    std::set<char> left, right, v;
-    std::set<char> uch;
-    for(auto c: line) uch.emplace(c);
-    badgedetector.push_back(uch);
-
-    for (int i = 0; i < (line.length()/2); i++) {
-      left.emplace(line[i]);
-      right.emplace(line[line.length()-i-1]);
+    std::set<char> cc;
+    badgedetector.push_back(line);
+    for (int i = 0; i < line.length()/2; i++) cc.emplace(line[i]);
+    for (int i = line.length()/2; i < line.length(); i++) {
+      if (cc.find(line[i]) != cc.end()) {
+        res.first += priorities.find(line[i]);  
+        cc.erase(line[i]);
+      }
     }
 
-    std::set_intersection(left.begin(), left.end(), right.begin(), right.end(), std::inserter(v, v.end()));
-    for(auto a: v) res.first += priorities.find(a);
-
     if (badgedetector.size() == 3) {
-      std::map<char,int> pp;
-      for(int i = 0; i < 3; i++) for(auto c: badgedetector.at(i)) pp[c]++;
-      for (auto p: pp) if (p.second == 3) res.second += priorities.find(p.first);
-      pp.clear();
+      std::set<char> pp, po;
+      for (int i = 0; i < badgedetector[0].length(); i++) pp.emplace(badgedetector[0][i]);
+      for (int i = 0; i < badgedetector[1].length(); i++) if (pp.find(badgedetector[1][i]) != pp.end()) po.emplace(badgedetector[1][i]);
+      for (int i = 0; i < badgedetector[2].length(); i++) if (po.find(badgedetector[2][i]) != po.end()) { res.second += priorities.find(badgedetector[2][i]); break; }
       badgedetector.clear();
     }
   }

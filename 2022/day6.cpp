2d75d201ffea360eb0e6443f48fb223ad5508e47
Timo <ethikka+bitbucket@gmail.com>
2022-12-06 07:19:07 +0100

    Day 6: Tuning Trouble

---
+++ 2022/day6.cpp
@@ -0,0 +1,20 @@
+#include <sstream>
+#include <vector>
+#include <set>
+#include "../common/lib.h"
+
+std::pair<std::uintmax_t,std::uintmax_t> solve() {
+  std::pair<std::uintmax_t,std::uintmax_t> res;
+  char c;
+  std::vector<char> chars;
+  while (std::cin >> c) chars.push_back(c);
+  for (int i = 0; i < chars.size()-3 && (res.first == 0 || res.second == 0); i++) {
+    std::set<char> test;
+    for (int x = 0; x < 14; x++) {
+      test.emplace(chars.at(i+x));
+      if (x == 3 && test.size() == 4 && res.first == 0) res.first = i+4;
+    }
+    if (test.size() == 14 && res.second == 0) res.second = i+14;
+  }
+  return res;
+}

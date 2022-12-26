346085dcd9b076b5598cbfdbace91ff1f5a9bf83
Timo <ethikka+bitbucket@gmail.com>
2022-12-26 23:22:51 +0100

    Day 17: Pyroclastic Flow

--- 2022/day17.cpp
+++ 2022/day17.cpp
@@ -1,71 +1,48 @@
 #include <sstream>
-#include <vector>
 #include <map>
+#include <array>
+#include <vector>
 #include "../common/lib.h"
+#include "../common/vector2.h"
 #include "../common/circularlist.h"
 
-struct pos { int x; int y;};
+tbb::circularlist<char> jets;
 
-struct block {
-  std::vector<uint8_t> enc;
-  block(std::string inp) {
-    std::string tmp(inp);
-    while (tmp.find('|') != std::string::npos) {
-      int pos = tmp.find('|');
-      pixels.push_back(tmp.substr(0, pos));
-      tmp = tmp.substr(pos+1);
-    }
-    pixels.push_back(tmp);
-    for (auto p: pixels) {
-      uint8_t m(1), r(0);
-      for (int i = 0; i < p.size(); i++) { if (p[i] == '#') r += m; m *= 2; }
-      enc.push_back(r);
-    }
-  };
-  std::vector<std::string> pixels;
-  std::string offset(int o) {
-    std::string res;
-    for(uint8_t c: enc) res += (c << o);
-    return res;
-  }
-};
+typedef std::vector<vector2> Rock;
 
-struct chamber {
-  chamber() { shaft = (char)127; /* bodem */ }
-  int64_t height() { return 0; };
-  std::string shaft;
+void move_to_starting_height(Rock& rock, int highest) { for (auto& ele : rock) ele.y += highest + 3; }
+void move(Rock& rock) { for (auto& ele : rock) ele.y--; }
 
-  void simulate_block(block b, tbb::circularlist<char> &jets) {
-    pos p = {2, static_cast<int>(b.pixels.size()+2+shaft.size())};
-    bool collision(false);
-    while (!collision) {
-      switch (jets.front_and_next()) {
-        case '<': if (p.x > 0) p.x--; break;
-        case '>': if (p.x < (7-b.pixels[0].size())) p.x++; break;
-      }
-      auto bo = b.offset(p.x);
-      std::cout << p.y << std::endl;
-      if (p.y < 0) return;
+bool intersection(const std::vector<std::array<char, 7>>& chamber, const Rock& rock) {
+  for (const auto& ele : rock) if (ele.y < chamber.size()) if (chamber[ele.y][ele.x] == '#') return true;
+  return false;
+}
 
-      if ((p.y+b.pixels.size()-1) <= shaft.size()) {
-        int cc(b.pixels.size());
-        collision = true;
-        for (char c: bo) {
-          collision &= ((c && shaft[p.y-(cc--)]) == 0);
+void add_rock_to_chamber(std::vector<std::array<char, 7>>& chamber, const Rock& rock) {
+  for (const auto& ele : rock) while(chamber.size() <= ele.y) chamber.push_back({'.','.','.','.','.','.','.'});
+  for (const auto& ele : rock) chamber[ele.y][ele.x] = '#';
+}
+
+void apply_jet(Rock& rock, char jet, const std::vector<std::array<char, 7>>& chamber) {
+  const auto prev = rock;
+  switch (jet) {
+    case '>': {
+        for (auto& ele : rock) {
+          ele.x++;
+          if (ele.x > 6) { rock = prev; return; }
         }
       }
-
-      // can move down?
-      if (collision) {
-        std::cout << "Krak " << p.y << std::endl;
-        for (char c: bo) 
-          shaft[p.y] |= c;
-        // put block in shaft at current pos
-        return;
-      } else p.y--;
+      break;
+    case '<': {
+        for (auto& ele : rock) {
+          ele.x--;
+          if (ele.x < 0) { rock = prev; return; }
+        }
+      }
+      break;
     }
-  };
-};
+  if (intersection(chamber, rock)) rock = prev;
+}
 
 std::pair<std::uintmax_t,std::uintmax_t> solve() {
   std::pair<std::uintmax_t,std::uintmax_t> res;
@@ -73,15 +50,47 @@ std::pair<std::uintmax_t,std::uintmax_t> solve() {
   char c;
   while (std::cin >> c) jets.enqueue(c);
 
-  tbb::circularlist<block> blocks;
-  blocks.enqueue({block("####"),block(".#.|###|.#."),block("..#|..#|###"),block("#|#|#|#"),block("##|##")});
+  tbb::circularlist<Rock> rocks;
+  rocks.enqueue({
+    Rock{vector2(0,0), vector2(1,0), vector2(2,0), vector2(3,0)},
+    Rock{vector2(1,0), vector2(0,1), vector2(1,1), vector2(2,1), vector2(1,2)},
+    Rock{vector2(2,2), vector2(2,1), vector2(0,0), vector2(1,0), vector2(2,0)},
+    Rock{vector2(0,0), vector2(0,1), vector2(0,2), vector2(0,3)},
+    Rock{vector2(0,0), vector2(1,0), vector2(0,1), vector2(1,1)}
+  });
 
-  chamber ch;
+  for (auto& rock : rocks.buffer) for (auto& ele : rock) ele.x += 2;
 
-  int r(0);
-  while (r++ < 5/*2022*/) ch.simulate_block(blocks.front_and_next(), jets);
-  res.first = ch.height();
-//  while (r++ < 2022) ch.simulate_block(blocks.front_and_next(), jets);
-//  res.second = ch.height();
+  std::map<int64_t,std::vector<std::pair<int64_t,int64_t>>> states;
+
+  int rock_count = 0;
+  std::vector<std::array<char, 7>> chamber;
+  chamber.push_back({'#','#','#','#','#','#','#'}); // bottom
+  while (rock_count++ < 6025) {
+    auto rock = rocks.front_and_next();
+    move_to_starting_height(rock, chamber.size());
+    auto prev = rock;
+    while (!intersection(chamber, rock)) {
+      apply_jet(rock, jets.front_and_next(), chamber);
+      prev = rock;
+      move(rock);
+    }
+    add_rock_to_chamber(chamber, prev);
+    states[jets.internal_index()+(rocks.internal_index()*100000)].push_back({chamber.size(), rock_count});
+    if (rock_count == 2022) res.first = chamber.size()-1;
+  }
+
+  auto ss = states.at(jets.internal_index()+(rocks.internal_index()*100000));
+  int ROCKCYCLE = ss.back().first;
+  int ROCKHEIGTH = ss.back().second;
+  ss.pop_back();
+  ROCKCYCLE -= ss.back().first;
+  ROCKHEIGTH -= ss.back().second;
+
+  int64_t cnt(0), ln(1000000000000);
+  while (ln > 5100) { ln -= ROCKHEIGTH; cnt++; }
+
+  // find state with rockcount ln
+  for (auto s: states) for (auto ss: s.second) if (ss.second == ln) res.second = (ss.first+(ROCKCYCLE*cnt))-1;
   return res;
 }

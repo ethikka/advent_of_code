4d8254809a64fe3ccc040120146b7693aa2165da
Timo Bredenoort <timo.bredenoort@iddinkgroup.com>
2023-12-04 08:56:47 +0100

    ...

--- 2023/day2.cpp
+++ 2023/day2.cpp
@@ -9,7 +9,7 @@ int a(std::istringstream input) {
     input >> gamenr >> gamenr;
     while (input >> num >> color) 
         if ((color[0] == 'r' && num > 12) || (color[0] == 'g' && num > 13) || (color[0] == 'b' && num > 14)) return 0;
-    return std::stoi(gamenr.substr(0, gamenr.size()-1));;
+    return std::stoi(gamenr.substr(0, gamenr.size()-1));
 }
 
 int b(std::istringstream input) {
@@ -24,12 +24,9 @@ int b(std::istringstream input) {
 std::pair<std::uintmax_t,std::uintmax_t> solve() {
   std::pair<std::uintmax_t,std::uintmax_t> res;
   std::string line;
-  while (!std::cin.eof()) { 
-    std::getline(std::cin, line);
+  while (std::getline(std::cin, line)) { 
     res.first += a(std::istringstream(line));
     res.second += b(std::istringstream(line));
-//    res.second += (find_num(line, true, true)*10)+find_num(line, true, false);
   }
   return res;
-
 }
--- 2023/day4.cpp
+++ 2023/day4.cpp
@@ -9,26 +9,26 @@
 std::pair<std::uintmax_t,std::uintmax_t> solve() {
   std::pair<std::uintmax_t,std::uintmax_t> res;
   std::map<int,int> stackssize;
-  int num(0),score(0);
   std::string line;
-  int counter(1);
+  int counter;
   while (std::getline(std::cin, line)) {
     std::istringstream ssl(line);
+    int score(0), num;
     std::set<int> numbers;
     ssl.ignore(10);
     std::copy_n(std::istream_iterator<int>(ssl), 10, std::inserter(numbers, numbers.begin()));    
     ssl.ignore(3);
-    stackssize[counter] = std::max(1, stackssize[counter]+1);
+
+    stackssize[counter] = std::max(1, stackssize[++counter]+1);
 
     int wins(0);
     while (ssl >> num) {
         if (numbers.count(num) > 0) {
-            stackssize[counter+ ++wins] = std::max(stackssize[counter], stackssize[counter+wins]+stackssize[counter]);
-            score = (score == 0) ? 1 : score * 2;
+            stackssize[counter+wins] = std::max(stackssize[counter], stackssize[++wins+counter]+stackssize[counter]);
+            score = std::max(1, score*2);
         }
     }
     res.first += score;
-    score = 0; counter++;
   }
 
   for(auto ss: stackssize) res.second += ss.second;

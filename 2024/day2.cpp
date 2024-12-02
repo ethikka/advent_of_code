f7210e83a6b579f66ec083b32546ee9e2e16f86c
Timo Bredenoort <timo.bredenoort@iddinkgroup.com>
2024-12-02 08:15:13 +0100

    cleanup

--- 2024/day2.cpp
+++ 2024/day2.cpp
@@ -3,48 +3,38 @@
 #include "../common/lib.h"
 
 class report {
+  private:
     std::vector<int> reports;
-  public:
-    void add(int n) { reports.push_back(n); }
-    bool valid(std::vector<int> rr) {
-      bool rising = rr.at(0) < rr.at(1);
+    bool _valid(std::vector<int> rr) {
       bool valid = true;
       for (int i = 1; i < rr.size(); i++) {
-        int diff = rr.at(i-1)-rr.at(i);
-        valid &= rising ? diff < 0 : diff > 0;
-        diff = std::abs(diff);
+        valid &= ((rr.at(0) - rr.at(1)) ^ (rr.at(i-1)-rr.at(i))) >= 0;
+        int diff = std::abs(rr.at(i-1)-rr.at(i));
         valid &= ((diff > 0) && (diff < 4));
       }
       return valid;
     };
-    bool valid_a() { return valid(reports); }
+  public:
+    report(std::string input) { std::istringstream ssl(input); while (!ssl.eof()) { int ii; ssl >> ii; reports.push_back(ii); }}
+
+    bool valid_a() { return _valid(reports); }
     bool valid_b() {
-      if (valid(reports)) return true;
+      if (_valid(reports)) return true;
       for (int i = 0; i < reports.size(); i++) {
         std::vector<int> subr;
         for (int j = 0; j < reports.size(); j++) if (i != j) subr.push_back(reports.at(j));
-        if (valid(subr)) return true;
+        if (_valid(subr)) return true;
       }
       return false;
     };
-
   };
-
-std::vector<report> reports;
 
 std::pair<std::uintmax_t,std::uintmax_t> solve() {
   std::pair<std::uintmax_t,std::uintmax_t> res;
   std::string line;
-  while (std::getline(std::cin, line)) {
-    report r;
-    std::istringstream ssl(line);
-    while (!ssl.eof()) {
-      int ii;
-      ssl >> ii;
-      r.add(ii);
-    }
-    reports.push_back(r);
-  }
+  std::vector<report> reports;
+
+  while (std::getline(std::cin, line)) reports.push_back(line);
 
   for(auto rr: reports) {
     if (rr.valid_a()) res.first++;

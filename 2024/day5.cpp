3fa5ab146fb5c7cab0e9e8c6ac0b803a9c9ac11a
Timo Bredenoort <timo.bredenoort@iddinkgroup.com>
2024-12-05 08:35:51 +0100

    --- Day 5: Print Queue ---

--- 2024/day5.cpp
+++ 2024/day5.cpp
@@ -1,10 +1,43 @@
 #include <sstream>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
 #include "../common/lib.h"
 
 std::pair<std::uintmax_t,std::uintmax_t> solve() {
   std::pair<std::uintmax_t,std::uintmax_t> res;
   std::string line;
-  while (std::cin >> line) {
+
+  std::map<int,std::set<int>> rules;
+
+  while (std::getline(std::cin,line) && line.size() > 0) {
+    int spl = line.find('|');
+    rules[std::stoi(line.substr(spl+1))].emplace(std::stoi(line.substr(0,spl)));
   }
+
+  while (std::getline(std::cin,line) && line.size() > 0) {
+    std::istringstream ssl(line);
+    std::vector<int> upd;
+    int num;
+    char discard;
+    while (!ssl.eof()) {
+      ssl >> num >> discard;
+      upd.push_back(num);
+    }
+
+    bool valid = true;
+    for(int i = 0; i < upd.size()-1; i++) valid &= (rules[upd.at(i+1)].find(upd.at(i)) != rules[upd.at(i+1)].end());
+
+    if (valid) res.first += upd[(upd.size()-1)/2];
+    else {
+      std::map<int,int> fc, cf;
+      for(auto i: upd) { fc[i] = 0; for (auto e: rules[i]) if (std::find(upd.begin(), upd.end(), e) != upd.end()) fc[i]++; }
+      for (auto e: fc) cf[e.second] = e.first;
+      auto it = cf.begin();
+      res.second += std::next(it, (cf.size()-1)/2)->second;
+    }
+  }
+
   return res;
 }

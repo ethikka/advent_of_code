25d82d06faa2ce360c129e9c36bcad189f5d5341
Timo Bredenoort <timo.bredenoort@iddinkgroup.com>
2024-12-09 12:45:05 +0100

    optimizations

--- 2024/day6.cpp
+++ 2024/day6.cpp
@@ -12,7 +12,7 @@ struct work {
     direction d;
 };
 
-int64_t sol_a(tbb::grid2d<char>& grid) {
+std::set<vector2<int>> sol_a(tbb::grid2d<char>& grid) {
     vector2<int> guard = grid.find_elements('^')[0];
     direction d = north;
 
@@ -31,49 +31,48 @@ int64_t sol_a(tbb::grid2d<char>& grid) {
         }
         if (grid.in_bounds(np)) q.emplace(work{np, nd});
     };
-    return visited.size();
+    return visited;
 };
 
-int64_t sol_b(tbb::grid2d<char>& grid) {
+int64_t sol_b(tbb::grid2d<char>& grid, std::set<vector2<int>> n) {
     int64_t pp = 0;
     vector2<int> guard = grid.find_elements('^')[0];
     direction d = north;
 
-    for (int xx = 0; xx <= grid._x; xx++)
-        for (int yy = 0; yy <= grid._y; yy++) {
-            if (grid.get_element({xx, yy}).second == '.') {
-                grid.place_element({xx, yy}, '#');
+    for (auto e : n) {
+        grid.place_element(e, '#');
 
-                std::set<vector2<int>> visited[4];
-                std::queue<work> q;
-                q.emplace(work{guard, d});
-                while (!q.empty()) {
-                    auto w = q.front();
-                    q.pop();
-                    vector2<int> np = w.p + offsets[(w.d) % 4];
-                    direction nd = w.d;
-                    if (grid.get_element(np).second == '#') {
-                        if (visited[w.d].count(w.p)) {
-                            pp++;
-                            grid.remove_element({xx, yy});
-                            continue;
-                        }
-                        visited[w.d].emplace(w.p);
-                        while (grid.get_element(np).second == '#') {
-                            nd = direction((nd + 1) % 4);
-                            np = w.p + offsets[(nd) % 4];
-                        }
-                    };
-                    if (grid.in_bounds(np)) q.emplace(work{np, nd});
-                };
-                grid.remove_element({xx, yy});
-            }
-        }
+        std::set<vector2<int>> visited[4];
+        std::queue<work> q;
+        q.emplace(work{guard, d});
+        while (!q.empty()) {
+            auto w = q.front();
+            q.pop();
+            vector2<int> np = w.p + offsets[(w.d) % 4];
+            direction nd = w.d;
+            if (grid.get_element(np).second == '#') {
+                if (visited[w.d].count(w.p)) {
+                    pp++;
+                    grid.remove_element(e);
+                    continue;
+                }
+                visited[w.d].emplace(w.p);
+                while (grid.get_element(np).second == '#') {
+                    nd = direction((nd + 1) % 4);
+                    np = w.p + offsets[(nd) % 4];
+                }
+            };
+            if (grid.in_bounds(np)) q.emplace(work{np, nd});
+        };
+        grid.remove_element(e);
+    }
     return pp;
 };
 
 std::pair<std::uintmax_t, std::uintmax_t> solve() {
     tbb::grid2d<char> grid;
     grid.read('.');
-    return {sol_a(grid), sol_b(grid)};
+
+    auto ss = sol_a(grid);
+    return {ss.size(), sol_b(grid, ss)};
 }
--- 2024/day9.cpp
+++ 2024/day9.cpp
@@ -3,14 +3,9 @@
 
 #include "../common/lib.h"
 
-struct FileInfo {
-    int64_t id = 0;
-    int blocks;
-};
-
 int64_t crc(std::string input, bool part_b) {
     std::istringstream ssl(input);
-    std::vector<FileInfo> map;
+    std::vector<std::pair<int64_t, int>> map;
     char c;
 
     int64_t id = 0;
@@ -21,25 +16,24 @@ int64_t crc(std::string input, bool part_b) {
     }
 
     for (int64_t i = map.size() - 1; i >= 0; i--) {
-        if (map[i].id == -1) continue;
+        if (map[i].first == -1) continue;
 
         for (size_t j = 0; j < i; j++) {
-            if (map[j].id != -1 || (part_b && map[j].blocks < map[i].blocks))
+            if (map[j].first != -1 || (part_b && map[j].second < map[i].second))
                 continue;
 
-            if (map[j].blocks == map[i].blocks)
+            if (map[j].second == map[i].second)
                 std::swap(map[i], map[j]);
-            else if (map[j].blocks > map[i].blocks) {
-                int blocksLeft = map[j].blocks - map[i].blocks;
-                map[j].blocks = map[i].blocks;
+            else if (map[j].second > map[i].second) {
+                int blocksLeft = map[j].second - map[i].second;
+                map[j].second = map[i].second;
                 std::swap(map[i], map[j]);
                 map.insert(map.begin() + j + 1, {-1, blocksLeft});
             } else if (!part_b) {
-                int blocksLeft = map[i].blocks - map[j].blocks;
-                map[i].blocks = map[j].blocks;
+                int blocksLeft = map[i].second - map[j].second;
+                map[i].second = map[j].second;
                 std::swap(map[i], map[j]);
-                map.insert(map.begin() + i, {map[j].id, blocksLeft});
-                i++;
+                map.insert(map.begin() + i++, {map[j].first, blocksLeft});
             }
             break;
         }
@@ -47,11 +41,11 @@ int64_t crc(std::string input, bool part_b) {
 
     int64_t block = 0, out = 0;
     for (size_t i = 0; i < map.size(); i++)
-        if (map[i].id == -1)
-            block += map[i].blocks;
+        if (map[i].first == -1)
+            block += map[i].second;
         else
-            for (int j = 0; j < map[i].blocks; j++) out += map[i].id * block++;
-
+            for (int j = 0; j < map[i].second; j++)
+                out += map[i].first * block++;
     return out;
 }
 

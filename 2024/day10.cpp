0a43ebd95102c704c94c184cdd8a7b83700d24ff
Timo <ethikka+github@gmail.com>
2024-12-10 07:42:28 +0100

    --- Day 10: Hoof It ---

--- 2024/day10.cpp
+++ 2024/day10.cpp
@@ -1,10 +1,38 @@
 #include <sstream>
-#include "../common/lib.h"
+#include <queue>
+#include <map>
 
-std::pair<std::uintmax_t,std::uintmax_t> solve() {
-  std::pair<std::uintmax_t,std::uintmax_t> res;
-  std::string line;
-  while (std::cin >> line) {
-  }
-  return res;
+#include "../common/lib.h"
+#include "../common/grid2d.h"
+
+std::pair<int64_t, int64_t> count_paths(tbb::grid2d<int> g, vector2<int> start) {
+    std::queue<std::pair<vector2<int>, int>> q;
+    std::map<vector2<int>, int> ends;
+    q.emplace(std::pair<vector2<int>, int>{start, 0});
+    while (!q.empty()) {
+        auto c = q.front();
+        q.pop();
+
+        if (c.second == 9) ends[c.first]++;
+
+        for (auto o : offsets)
+            if (g.get_element(c.first + o).second == (c.second + 1))
+                q.emplace(std::pair<vector2<int>, int>{c.first + o, c.second + 1});
+    }
+    int res = 0;
+    for (auto e : ends) res += e.second;
+    return {ends.size(), res};
+}
+
+std::pair<std::uintmax_t, std::uintmax_t> solve() {
+    std::pair<std::uintmax_t, std::uintmax_t> res;
+    tbb::grid2d<int> g;
+    g.read();
+    for (auto e : g.find_elements(0)) {
+        auto r = count_paths(g, e);
+        res.first += r.first;
+        res.second += r.second;
+    }
+
+    return res;
 }

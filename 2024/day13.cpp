76f26a54c3ceaba96737a9e0dc473cf3d5d69557
Timo Bredenoort <timo.bredenoort@iddinkgroup.com>
2024-12-13 09:46:42 +0100

    cleanup and tweaks

--- 2024/day13.cpp
+++ 2024/day13.cpp
@@ -8,40 +8,35 @@ class claw {
    private:
     vector2<int64_t> a{0, 0};
     vector2<int64_t> b{0, 0};
-    vector2<int64_t> target{0, 0};
+    vector2<int64_t> t{0, 0};
 
    public:
     claw() {
+        // clang-format off
         std::string i;
-        std::cin >> i >> i >> i;
-        a.x = std::stoi(i.substr(2, i.size() - 3));
-        std::cin >> i;
-        a.y = std::stoi(i.substr(2));
-        std::cin >> i >> i >> i;
-        b.x = std::stoi(i.substr(2, i.size() - 3));
-        std::cin >> i;
-        b.y = std::stoi(i.substr(2));
-        std::cin >> i >> i;
-        target.x = std::stoi(i.substr(2, i.size() - 3));
-        std::cin >> i;
-        target.y = std::stoi(i.substr(2));
+        std::cin >> i >> i >> i; a.x = std::stoi(i.substr(2, i.size() - 3));
+        std::cin >> i;           a.y = std::stoi(i.substr(2));
+        std::cin >> i >> i >> i; b.x = std::stoi(i.substr(2, i.size() - 3));
+        std::cin >> i;           b.y = std::stoi(i.substr(2));
+        std::cin >> i >> i;      t.x = std::stoi(i.substr(2, i.size() - 3));
+        std::cin >> i;           t.y = std::stoi(i.substr(2));
+        // clang-format on
     };
+
     int64_t optimum(int64_t offset = 0) {
         // https://en.wikipedia.org/wiki/Cramer's_rule#Explicit_formulas_for_small_systems
         // __         __   __       __     __         --
-        // |  a.x  b.x |   | press_a |     |  target.x |
-        // |  a.y  b.y | x | press_b |  =  |  target.y |
+        // |  a.x  b.x |   | press_a |     |  t.x |
+        // |  a.y  b.y | x | press_b |  =  |  t.y |
         // --         --   --       --     --         --
-        int64_t det_norm = a.x * b.y - b.x * a.y;
-        int64_t det_x = (target.x + offset) * b.y - (target.y + offset) * b.x;
-        double x = (double)det_x / (double)det_norm;
-        int64_t det_y = a.x * (target.y + offset) - (target.x + offset) * a.y;
-        double y = (double)det_y / (double)det_norm;
 
-        const double EPSILON = std::pow(10, -8);
-        if (std::abs(x - std::llround(x)) > EPSILON || std::abs(y - std::llround(y)) > EPSILON) return 0;
-        if (x < 0 || y < 0) return 0;
-        return 3 * x + y;
+        double det_norm = (a.x * b.y - b.x * a.y) * 1.0;
+        double x = ((t.x + offset) * b.y - (t.y + offset) * b.x) * 1.0 / det_norm;
+        double y = ((t.y + offset) * a.x - (t.x + offset) * a.y) * 1.0 / det_norm;
+
+        // if either x or y are not an exact integer it's not valid
+        if (std::trunc(x) == x && std::trunc(y) == y) return 3 * x + y;
+        return 0;
     }
 };
 

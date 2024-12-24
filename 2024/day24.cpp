f9cf16a5bad028302c9b030f156d3e39268c1f5c
Timo <ethikka+github@gmail.com>
2024-12-24 17:26:15 +0100

    --- Day 24: Crossed Wires ---

--- 2024/day24.cpp
+++ 2024/day24.cpp
@@ -1,10 +1,54 @@
 #include <sstream>
+#include <vector>
+#include <map>
+
 #include "../common/lib.h"
 
-std::pair<std::uintmax_t,std::uintmax_t> solve() {
-  std::pair<std::uintmax_t,std::uintmax_t> res;
-  std::string line;
-  while (std::cin >> line) {
-  }
-  return res;
+struct instr {
+    std::string g1;
+    std::string g2;
+    std::string op;
+    std::string og;
+    std::string to_string() { return g1 + " " + op + " " + g2 + " -> " + og; }
+};
+
+int64_t sim(std::map<std::string, bool> reg, std::vector<instr> instructions) {
+    std::vector<instr> i(instructions);
+
+    auto it = i.begin();
+    while (i.size() > 0) {
+        if (reg.find(it->g1) != reg.end() && reg.find(it->g2) != reg.end()) {
+            if (it->op == "AND") reg[it->og] = (reg[it->g1] && reg[it->g2]);
+            if (it->op == "OR") reg[it->og] = (reg[it->g1] || reg[it->g2]);
+            if (it->op == "XOR") reg[it->og] = (reg[it->g1] != reg[it->g2]);
+            i.erase(it);
+            it = i.begin();
+        } else
+            it = std::next(it);
+    }
+    int64_t res(0), mul(1);
+    for (auto r : reg) {
+        if (r.first.starts_with('z')) {
+            res += (mul * r.second);
+            mul *= 2;
+        }
+    }
+    return res;
+};
+
+std::pair<std::uintmax_t, std::uintmax_t> solve() {
+    std::map<std::string, bool> reg;
+    std::vector<instr> instructions;
+    std::string g, init, op, g2, og;
+    while (std::cin >> g >> init)
+        if (g.ends_with(':'))
+            reg[g.substr(0, g.size() - 1)] = (init == "1");
+        else {
+            std::cin >> g2 >> og >> og;
+            instructions.push_back({(g < g2 ? g : g2), (g < g2 ? g2 : g), init, og});
+            continue;
+        }
+    while (std::cin >> g >> op >> g2 >> op >> og >> og) instructions.push_back({(g < g2 ? g : g2), (g < g2 ? g2 : g), op, og});
+
+    return {sim(reg, instructions), 0};
 }

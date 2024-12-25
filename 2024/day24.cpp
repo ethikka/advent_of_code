91a31de12b00955cbacea0a137066d26f87f08e6
Timo <ethikka+github@gmail.com>
2024-12-25 07:40:51 +0100

    crossed wires (part 2 non-paper version)

--- 2024/day24.cpp
+++ 2024/day24.cpp
@@ -1,6 +1,7 @@
 #include <sstream>
 #include <vector>
 #include <map>
+#include <set>
 
 #include "../common/lib.h"
 
@@ -9,8 +10,10 @@ struct instr {
     std::string g2;
     std::string op;
     std::string og;
-    std::string to_string() { return g1 + " " + op + " " + g2 + " -> " + og; }
+    std::string to_string() const { return g1 + " " + op + " " + g2 + " -> " + og; }
 };
+
+bool operator<(const instr a, const instr b) { return (a.to_string() < b.to_string()); }
 
 int64_t sim(std::map<std::string, bool> reg, std::vector<instr> instructions) {
     std::vector<instr> i(instructions);
@@ -36,6 +39,53 @@ int64_t sim(std::map<std::string, bool> reg, std::vector<instr> instructions) {
     return res;
 };
 
+std::set<std::string> rewire(std::set<instr> instructions, int count) {
+    std::set<std::string> res;
+    std::string _xor, _and;
+    // clang-format off
+    for (auto i : instructions)
+        if (i.g1.starts_with('x')) {
+            if (i.op == "XOR") {
+                if (i.og.starts_with('z')) res.emplace(i.og);
+                else                       _xor = i.og;  // ab
+            }
+            if (i.op == "AND") {
+                if (i.og.starts_with('z')) res.emplace(i.og);
+                else                       _and = i.og;  // cob
+            }
+        }
+
+    for (auto i : instructions)
+        if (!i.g1.starts_with('x')) {
+            if (i.op == "XOR") if (!i.og.starts_with('z')) res.emplace(i.og);
+            if (i.op == "AND") if (i.og.starts_with('z'))  res.emplace(i.og); 
+            if (i.op == "OR") {
+                if (i.og.starts_with('z')) res.emplace(i.og);
+                if (_xor == i.g1 || _xor == i.g2) { res.emplace(_and); res.emplace(_xor); }
+            }
+        }
+    // clang-format on
+    return res;
+}
+
+std::set<instr> get(std::vector<instr> instructions, int n) {
+    std::set<instr> res;
+    std::string nn = std::to_string(n);
+    if (nn.size() == 1) nn = "0" + nn;
+    for (auto i : instructions)
+        if (i.g1 == "x" + nn || i.g2 == "y" + nn) {
+            res.emplace(i);
+            for (auto ii : instructions)
+                if (ii.g1 == i.og || ii.g2 == i.og) {
+                    res.emplace(ii);
+                    if (ii.op == "AND")
+                        for (auto iii : instructions)
+                            if (iii.g1 == ii.og || iii.g2 == ii.og) res.emplace(iii);
+                }
+        }
+    return res;
+};
+
 std::pair<std::uintmax_t, std::uintmax_t> solve() {
     std::map<std::string, bool> reg;
     std::vector<instr> instructions;
@@ -49,6 +99,14 @@ std::pair<std::uintmax_t, std::uintmax_t> solve() {
             continue;
         }
     while (std::cin >> g >> op >> g2 >> op >> og >> og) instructions.push_back({(g < g2 ? g : g2), (g < g2 ? g2 : g), op, og});
+
+    std::set<std::string> res_b;
+    for (int i = 1; i < 44; i++)
+        for (auto s : rewire(get(instructions, i), i)) res_b.emplace(s);
+
+    std::string rb;
+    for (auto b : res_b) rb += b + ',';
+    std::cout << "Solution part 2: " << rb.substr(0, rb.size() - 1) << std::endl;
 
     return {sim(reg, instructions), 0};
 }
